9f756980d3245319a3552676459b7b6196cd3606
sikandarDevBunch <sikandar.hanif@devbunch.com>
2022-06-26 15:03:45 +0500

    final

--- js/script.js
+++ js/script.js
@@ -2,7 +2,6 @@ burger = document.querySelector('.burger');
 navbar = document.querySelector('.navbar');
 navList = document.querySelector('.nav-list');
 rightnav = document.querySelector('.rightnav');
-console.log(burger);
 burger.addEventListener('click',()=>{
     navbar.classList.toggle('h-nav');
     navList.classList.toggle('v-class');
